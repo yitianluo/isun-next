91b32b5877b8790b2f3342352161694e535dc207
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-07-12 12:16:49 +0800

    modify nabar

--- public/category.js
+++ public/category.js
@@ -1,26 +1,32 @@
 const categories = [
     {
         name:"Ripple Cup",
+        nameCN:"高端纸杯",
         image:"/products/ripple.jpg"
     },
     {
         name:"Double Cup",
+        nameCN:"双层纸杯",
         image:"/products/double.jpg"
     },
     {
         name:"Vending Machine Cup",
+        nameCN:"售货机用纸杯",
         image:"/products/vending.jpg"
     },
     {
         name:"Natural Cup",
+        nameCN:"自然材料纸杯",
         image:"/products/natural.jpg"
     },
     {
         name:"Special Cup",
+        nameCN:"特殊纸杯",
         image:"/products/special.jpg"
     },
     {
         name:"Accessories",
+        nameCN:"配件",
         image:"/products/accessory.jpg"
     }
 ]
--- public/manager.js
+++ public/manager.js
@@ -1,18 +1,24 @@
 const managersData = [
     {
         quote:"Our key values are Credit, Professional, Efficiency and Forever Keeping.",
+        quoteCN:"我们的四大核心价值是信誉、专业、效率和坚持不懈",
         image:"/managers/liang.jpg",
-        name: "Luo Liang, CEO"
+        name: "Luo Liang, CEO",
+        nameCN:"罗亮，CEO"
     },
     {
         quote:"Our goal is to be leading in the line and developing world-famous brand.",
+        quoteCN:"我们的目标是成为行业领军者并打造世界知名的中国品牌",
         image:"/managers/jean.jpg",
-        name:"Jean, Overseas Manager"
+        name:"Jean, Overseas Manager",
+        nameCN:"简，海外主管"
     },
     {
         quote:"Our job is to keep developing and creating the best paper cups for customers.",
+        quoteCN:"我们的职责是为消费者不断研发和生产一流质量的产品",
         image:"/managers/jiu.jpg",
-        name:"Jiu, CTO, Postdoc"
+        name:"Joe, CTO, Postdoc",
+        nameCN:"乔，CTO，博士后"
     }
 ]
 
