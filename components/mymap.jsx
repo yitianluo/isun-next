b650757eaccdffba32f878aaf74e79bc494d89ef
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-07-12 14:33:34 +0800

    map function try1 and copyright

--- components/mymap.jsx
+++ components/mymap.jsx
@@ -7,9 +7,19 @@ import {Map,Marker} from "react-amap";
 function MyMap(props){
 
     var pos = { longitude: 117.270705 , latitude: 31.86183 };
-
+    function mapSelector(){
+        console.log("choose map");
+        if /* if we're on iOS, open in Apple Maps */
+        ((navigator.platform.indexOf("iPhone") != -1) || 
+        (navigator.platform.indexOf("iPad") != -1) || 
+        (navigator.platform.indexOf("iPod") != -1))
+        window.open("maps://maps.google.com/maps?daddr=31.86183,117.270705&amp;ll=");
+        else{
+            window.open("https://maps.google.com/maps?daddr=31.86183,117.270705&amp;ll=");
+        }
+    }
     return(
-        <div className={styles.mymap}>
+        <div className={styles.mymap} onClick={mapSelector}>
             <div className={styles.mapDiv} id="map">
                 <Map center={pos} zoom="100">
                     <Marker center={pos}></Marker>
@@ -19,7 +29,7 @@ function MyMap(props){
             <div className={styles.infoDiv}>
                 <div className={styles.info}>
                     <img src="/icons/location-solid.svg"></img>
-                    {props.isEn?<p>NO.279 Anqing Road, Hefei, Anhui, China</p>:<p>安徽省合肥市安庆路279号中环汇景大厦</p>}
+                    {props.isEn?<p>Zhonghuan Huijing Mansion, NO.279 Anqing Road, Hefei, Anhui, China</p>:<p>安徽省合肥市安庆路279号中环汇景大厦</p>}
                 </div>
                 <div className={styles.info}>
                     <img src="/icons/phone-solid.svg"></img>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -26,7 +26,7 @@ function NavBar(props){
             </div>
 
             <div className={styles.mobileMenu}>
-                <svg onClick={handleMenu} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="var(--Main-Green)" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+                <svg onClick={handleMenu} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="var(--Main-Green)" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
                 { isSpan && <div className={styles.spanMenu}>
                     <p onClick={handleMenu}> <a href="#introduction">{props.isEn?"Introduction":"公司概况"}</a> </p>
                     <p onClick={handleMenu}> <a href="#products">{props.isEn?"Products":"产品列表"}</a> </p>
@@ -35,13 +35,11 @@ function NavBar(props){
             </div>
 
             <div className={styles.menuDiv}>
+            {/* <a href="geo:124.028582,-29.201930" target="_blank">Click here for map</a> */}
                 <p> <a href="#introduction">{props.isEn?"Introduction":"公司概况"}</a> </p>
                 <p> <a href="#products">{props.isEn?"Products":"产品列表"}</a> </p>
                 <p> <a href="#contact">{props.isEn?"Contact":"联系我们"}</a> </p>
             </div>
-            
-            
-
         </div>
     )
 }
