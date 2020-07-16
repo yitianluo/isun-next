763dbc091628c947b3a60257d6b26067a8b735c3
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-07-16 21:28:14 +0800

    delete get method

--- pages/admin.js
+++ pages/admin.js
@@ -2,48 +2,48 @@ import React from "react";
 import axios from "axios";
 const bodyParser = require('body-parser');
 
-function Admin({messages}){   
+function Admin(){   
 
-    function showMessage(message,index){
+    // function showMessage(message,index){
 
-        console.log(message);
+    //     console.log(message);
         
-        return(
-        <div key={index}>
-            <h1>{message.name}</h1>
-            <p>{message.title}</p>
-            <p>{message.email}</p>
-            <p>{message.mobile}</p>
-            <p>{message.company}</p>
-            <p>{message.address}</p>                
-            <p>{message.message}</p>                
-        </div>)
-    }
+    //     return(
+    //     <div key={index}>
+    //         <h1>{message.name}</h1>
+    //         <p>{message.title}</p>
+    //         <p>{message.email}</p>
+    //         <p>{message.mobile}</p>
+    //         <p>{message.company}</p>
+    //         <p>{message.address}</p>                
+    //         <p>{message.message}</p>                
+    //     </div>)
+    // }
     return(
         <div>
             <h1>Here's the data from database.</h1>
-            {messages.map(showMessage)}
+            {/* {messages.map(showMessage)} */}
         </div>
     )
 }
 
-export async function getStaticProps(){
+// export async function getStaticProps(){
 
-    const messages = await axios.get(process.env.VERCEL_URL+"/api/messages").then(function(response){
+//     const messages = await axios.get(process.env.VERCEL_URL+"/api/messages").then(function(response){
         
-        return response.data;
-    }).catch(function(error){
-        return error;
-    });
+//         return response.data;
+//     }).catch(function(error){
+//         return error;
+//     });
 
-    console.log(messages);
+//     console.log(messages);
 
-    return {
-        props:{
-            messages
-        }
-    }
+//     return {
+//         props:{
+//             messages
+//         }
+//     }
     
-}
+// }
 
 export default Admin;
