71251d970477d54ba033763205783ec7a09d7cb3
Yitian Luo <yitianluo@YitiandeMacBook-Pro.local>
2020-07-20 08:27:20 +0800

    change cxo

--- public/manager.js
+++ public/manager.js
@@ -9,16 +9,16 @@ const managersData = [
     {
         quote:"Our goal is to be leading in the line and developing world-famous brand.",
         quoteCN:"我们的目标是成为行业领军者并打造世界知名的中国品牌",
-        image:"/managers/jean.jpg",
-        name:"Jean, Overseas Manager",
-        nameCN:"简，海外主管"
+        image:"/managers/liang.jpg",
+        name: "Luo Liang, CEO",
+        nameCN:"罗亮，CEO"
     },
     {
         quote:"Our job is to keep developing and creating the best paper cups for customers.",
         quoteCN:"我们的职责是为消费者不断研发和生产一流质量的产品",
-        image:"/managers/jiu.jpg",
-        name:"Joe, CTO, Postdoc",
-        nameCN:"乔，CTO，博士后"
+        image:"/managers/liang.jpg",
+        name: "Luo Liang, CEO",
+        nameCN:"罗亮，CEO"
     }
 ]
 
